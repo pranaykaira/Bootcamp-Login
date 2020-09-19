731142af7f6aeae427746898c32c55f18e87c17b
mekalarajesh <mekalarajesh@mekalarajesh-HP>
2020-09-19 19:53:04 +0530

    loan-commit

---
+++ LoginAction/src/main/java/com/login/main/LoginActionApplication.java
@@ -0,0 +1,13 @@
+package com.login.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginActionApplication.class, args);
+	}
+
+}
